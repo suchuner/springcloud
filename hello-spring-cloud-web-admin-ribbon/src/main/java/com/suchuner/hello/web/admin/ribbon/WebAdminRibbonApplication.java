00bb9a7e0c1e6eee07e6860b9f91c3e608364f23
suchuner <suchuner@163.com>
2019-05-23 18:43:38 +0800

    update

---
+++ hello-spring-cloud-web-admin-ribbon/src/main/java/com/suchuner/hello/web/admin/ribbon/WebAdminRibbonApplication.java
@@ -0,0 +1,20 @@
+package com.suchuner.hello.web.admin.ribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author suchuner
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableHystrix
+@EnableHystrixDashboard
+public class WebAdminRibbonApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebAdminRibbonApplication.class,args);
+    }
+}
